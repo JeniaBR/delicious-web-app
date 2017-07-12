ad15765e4ab819181ecfa1dcfc65df158b79c99b
Jenia Brook <jeniabrook@gmail.com>
2017-07-12 19:25:47 +0300

    Playing with routing and templating

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ const router = express.Router();
 
 // Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  
+  // res.send('Hey! It works!');
+  //res.json(req.query);
+  res.render('hello', {
+    dog: req.query.dog
+  });
+});
+
+router.get('/reverse/:name', (req, res) => {
+  const reverse = [...req.params.name].reverse().join('');
+  res.send(reverse);
 });
 
 module.exports = router;
