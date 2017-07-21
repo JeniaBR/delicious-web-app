ef29b624ad63b1c8d0ea8d85da12a9aca91fdb64
Jenia Brook <jeniabrook@gmail.com>
2017-07-21 16:55:55 +0300

    Add login page Add userController to handle the login proccess Add User schema to store the users in mongoDB

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+mongoose.Promise = global.Promise;
+const md5 = require('md5');
+const validator = require('validator');
+const mongodvErrorHandler = require('mongoose-mongodb-errors');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const userSchema =  new Schema({
+  email: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate: [validator.isEmail, 'Invalid Email Address'],
+    required: 'Please Supply an email address'
+  },
+  name: {
+    type: String,
+    required: 'Please suplly a name',
+    trim: true
+  }
+});
+
+userSchema.plugin(passportLocalMongoose, { usernameField: 'email' });
+userSchema.plugin(mongodvErrorHandler);
+
+module.exports = mongoose.model('User', userSchema);
