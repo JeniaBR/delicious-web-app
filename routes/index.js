8131fbd97d2f8dc032a5e34b1eea65b19b5ae45b
Jenia Brook <jeniabrook@gmail.com>
2017-07-22 16:43:55 +0300

    Add middleware on '/add' route, so only signed in user can add new stores :)

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,13 @@ const { catchErrors } = require('../handlers/errorHandlers');
 // Do work here
 router.get('/',catchErrors(storeController.getStores));
 router.get('/stores',catchErrors(storeController.getStores));
-router.get('/add',storeController.addStore);
+
+// before we add a new store, the user have to be loggen in.
+// authController.isLoggedIn middleware will ensure that
+router.get('/add',
+  authController.isLoggedIn,
+  storeController.addStore
+);
 
 router.post('/add', 
   storeController.upload,
