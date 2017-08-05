abc296b84b354d67b5c95d46178b116dae23c322
Jenia Brook <jeniabrook@gmail.com>
2017-08-05 22:37:10 +0300

    Implemented Top stores route with advanced aggregation Add topStores.pug to present the 10 top stores by average review

--- models/Store.js
+++ models/Store.js
@@ -74,7 +74,34 @@ storeSchema.statics.getTagsList = function() {
     {$group: {_id: '$tags', count: {$sum: 1}}},
     {$sort: {count: -1}}
   ]);
-}
+};
+
+storeSchema.statics.getTopStores = function() {
+  return this.aggregate([
+    // Lookup Stores and populate their reviews
+    { $lookup: {
+        from: 'reviews', 
+        localField: '_id', 
+        foreignField: 'store', 
+        as: 'reviews'}
+      },
+    // Filter for only items that have 2 or more reviews
+    { $match: { 'reviews.1': { $exists: true } } },
+    // Add the average reviews field
+    { $project: {
+      photo: '$$ROOT.photo',
+      name: '$$ROOT.name',
+      reviews: '$$ROOT.reviews',
+      slug: '$$ROOT.slug',
+      averageRating: { $avg: '$reviews.rating'}
+    }},
+    // Sort it by our new field, highest first
+    { $sort: { averageRating: -1 } },
+    // limit to ar most 10
+    { $limit: 10 }
+  ]);
+};
+
 
 // find reviews where the stores _id property === reviews store property
 storeSchema.virtual('reviews', {
@@ -82,5 +109,13 @@ storeSchema.virtual('reviews', {
   localField: '_id', // which field on the store?
   foreignField: 'store' // which field on the review?
 });
+
+function autopopulate(next) {
+  this.populate('reviews');
+  next();
+}
+
+storeSchema.pre('find', autopopulate);
+storeSchema.pre('findOne', autopopulate);
 
 module.exports = mongoose.model('Store', storeSchema);
