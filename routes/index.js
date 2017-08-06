8749b86fc3538fc43b0dc466cfdc15352489a3ef
Jenia Brook <jeniabrook@gmail.com>
2017-08-06 17:53:24 +0300

    Implemented pagination

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ const { catchErrors } = require('../handlers/errorHandlers');
 // Do work here
 router.get('/',catchErrors(storeController.getStores));
 router.get('/stores',catchErrors(storeController.getStores));
+router.get('/stores/page/:page',catchErrors(storeController.getStores));
 
 // before we add a new store, the user have to be loggen in.
 // authController.isLoggedIn middleware will ensure that
